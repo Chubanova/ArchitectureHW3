38cac769bfcb011777dad398b7b91b4eb998448c
Chubanova <m.esaeva@inbox.ru>
2023-07-10 19:48:35 +0300

    Initial commit

---
+++ src/test/java/com/chubanova/MainClass.java
@@ -0,0 +1,15 @@
+package com.chubanova;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+    @Test
+    public void dummy() {
+        assertTrue(true);
+    }
+
+
+}
